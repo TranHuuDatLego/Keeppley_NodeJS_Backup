d6f64f0387337a6eeada88ba4ca3a79db33b8f43
TranHuuDatLego <144804545+TranHuuDatLego@users.noreply.github.com>
2024-11-11 20:12:35 +0700

    add countryList

--- updateInformation.js
+++ updateInformation.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('./connectDB');
+const countryList = require('./countryList'); // Đường dẫn đến file countryList.js
 
 router.post('/update-information', (req, res) => {
     const { userID, bio, birthday, country, phone } = req.body;
-    
+
     const sql = "UPDATE User SET bio = ?, birthday = ?, country = ?, phone = ? WHERE userID = ?";
     conn.query(sql, [bio, birthday, country, phone, userID], (err) => {
         if (err) {
@@ -12,7 +13,12 @@ router.post('/update-information', (req, res) => {
             console.error(err);
             return res.status(500).send("Error updating information.");
         }
-        res.send("Information updated successfully.");
+        // Thông báo thành công
+        error_message = ''
+        success_message = "Cập nhật thông tin thành công!";
+        // Cập nhật ảnh trong session
+        const website = 'Password.ejs';
+        res.render('Information', { userLogin: req.session.userLogin, countryList, error_message, success_message, website });
     });
 });
 
